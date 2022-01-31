00b8db66abe159f59e8e0f3cd8bcaf9ff13a4134
mostafakamal22 <36553185+mostafakamal22@users.noreply.github.com>
2022-01-31 21:15:55 +0200

    updating home with new projects and features

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,6 +2,8 @@
 const aside = document.getElementsByTagName("aside")[0] 
 const closeBtn = document.getElementById('close')
 const openBtn = document.getElementById('open')
+const navbar = document.getElementById('navbar')
+const aboutSection = document.getElementById('about')
 
 //opening aside when clicking open btn
 openBtn.addEventListener("click", ()=>{
@@ -14,3 +16,21 @@ closeBtn.addEventListener("click", ()=>{
     aside.style.visibility= "hidden"
     aside.style.opacity= '0'
 })
+
+//detecting about section's top value
+
+const detectTop = ()=> {
+    const top = document.getElementById('about').getBoundingClientRect().top
+    return top;
+}
+
+//changing navbar's style when scrolling
+window.addEventListener('scroll',()=>{
+    const topValue = detectTop()
+
+    if(topValue<0){
+        navbar.classList.add('navbar-active')
+    }else{
+        navbar.classList.remove('navbar-active')
+    }
+}) 
