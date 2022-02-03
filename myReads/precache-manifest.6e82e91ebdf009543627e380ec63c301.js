6b60fed2c7ba957d41c7cab2ace724b476fa2551
mostafakamal22 <36553185+mostafakamal22@users.noreply.github.com>
2022-02-03 16:38:06 +0200

    add myReads submodule

---
+++ myReads/precache-manifest.6e82e91ebdf009543627e380ec63c301.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "af4af1358dfbac3e3998",
+    "url": "/myReads/static/css/main.f859bea9.chunk.css"
+  },
+  {
+    "revision": "af4af1358dfbac3e3998",
+    "url": "/myReads/static/js/main.af4af135.chunk.js"
+  },
+  {
+    "revision": "c1f6b0303e5a62b1c21e",
+    "url": "/myReads/static/js/1.c1f6b030.chunk.js"
+  },
+  {
+    "revision": "27d1eb6964042f3c50bc",
+    "url": "/myReads/static/js/runtime~main.27d1eb69.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/myReads/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/myReads/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/myReads/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "0174ccf51fa0e1c2a1757a8413600a8e",
+    "url": "/myReads/index.html"
+  }
+];
