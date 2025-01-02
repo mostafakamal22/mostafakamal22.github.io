4419faa56be3e59ad39b45f2b9c3043187c2eb27
mostafakamal22 <mk1319@fayoum.edu.eg>
2025-01-02 22:33:11 +0200

    add fontawesome icons to the project

--- start-framework/src/App.jsx
+++ start-framework/src/App.jsx
@@ -1,3 +1,11 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+
 export default function App() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <FontAwesomeIcon icon={faStar} size="6x" />
+      Homesd
+    </div>
+  );
 }
