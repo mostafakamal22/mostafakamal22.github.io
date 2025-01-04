2929d6267436206c7a59c2445d7eb2d5dada2a76
mostafakamal22 <mk1319@fayoum.edu.eg>
2025-01-04 22:59:01 +0200

    add contact component initial setup

--- start-framework/src/components/Contact/Contact.jsx
+++ start-framework/src/components/Contact/Contact.jsx
@@ -1,3 +1,56 @@
+import PageHeader from "../Shared/PageHeader";
+import SubHeader from "../Shared/SubHeader";
+
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <section className="portfolio bg-light min-vh-100 py-5 d-flex flex-column align-items-center justify-content-center">
+      <div className="container text-center py-5 my-5">
+        <header>
+          <PageHeader title={"conatct section"} />
+          <SubHeader color={"--second-color"} />
+        </header>
+
+        <form className="w-50 p-3 mx-auto mt-5 text-start">
+          <label htmlFor="userName" className="position-relative top-0">
+            User Name :
+          </label>
+          <input
+            id="userName"
+            type="text"
+            placeholder="Type your name"
+            name="userName"
+            className="form-control border border-bottom py-3 position-relative mb-4"
+          />
+          <label htmlFor="userAge" className="position-relative top-0">
+            User Age :
+          </label>
+          <input
+            id="userAge"
+            type="text"
+            placeholder="type your age"
+            name="userName"
+            className="form-control border border-bottom py-3 position-relative mb-4"
+          />
+          <label htmlFor="userEmail" className="position-relative top-0">
+            User Email :
+          </label>
+          <input
+            id="userEmail"
+            type="text"
+            placeholder="Type your email"
+            name="userName"
+            className="form-control border border-bottom py-3 position-relative mb-4"
+          />
+
+          <button
+            className="btn mt-4 text-white "
+            style={{ backgroundColor: "var(--main-color)" }}
+            type="button"
+          >
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 }
