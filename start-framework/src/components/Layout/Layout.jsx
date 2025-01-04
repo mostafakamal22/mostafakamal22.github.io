87cc3648d272caf5cf50d041d12bd3f3aa3b7765
mostafakamal22 <mk1319@fayoum.edu.eg>
2025-01-04 20:24:08 +0200

    add document title change with routes handler

--- start-framework/src/components/Layout/Layout.jsx
+++ start-framework/src/components/Layout/Layout.jsx
@@ -1,8 +1,41 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
+import { useEffect } from "react";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
 
 export default function Layout() {
+  const location = useLocation();
+
+  useEffect(() => {
+    handleLocationChange();
+
+    function handleLocationChange() {
+      const currentPath = location.pathname;
+
+      switch (currentPath) {
+        case "/":
+          document.title = "Home";
+          break;
+
+        case "/about":
+          document.title = "About";
+          break;
+
+        case "/portfolio":
+          document.title = "Portfolio";
+          break;
+
+        case "/contact":
+          document.title = "Contact";
+          break;
+
+        default:
+          document.title = "Page Not Found";
+          break;
+      }
+    }
+  }, [location]);
+
   return (
     <>
       <Navbar />
