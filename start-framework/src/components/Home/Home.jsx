fa6eebbaa0222289b1d159b9b0ee8af05ea151cf
mostafakamal22 <mk1319@fayoum.edu.eg>
2025-01-04 21:09:31 +0200

    finish home section component

--- start-framework/src/components/Home/Home.jsx
+++ start-framework/src/components/Home/Home.jsx
@@ -1,3 +1,21 @@
+import Pageheader from "../Shared/PageHeader";
+import Subheader from "../Shared/SubHeader";
+import Avatar from "../../assets/images/avataaars.svg";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <section className="home min-vh-100 py-5 d-flex flex-column align-items-center justify-content-center">
+      <header className="text-white text-center py-5 my-5">
+        <img
+          src={Avatar}
+          alt="avatar"
+          className="w-100 mb-3"
+          style={{ maxWidth: "250px" }}
+        />
+        <Pageheader title={"start Framework"} />
+        <Subheader color={"white"} />
+        <div>Graphic Artist - Web Designer - Illustrator</div>
+      </header>
+    </section>
+  );
 }
