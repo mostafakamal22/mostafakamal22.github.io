91ac9dc166c626157c6fa0f16cd29ab4e333ab7a
mostafakamal22 <mk1319@fayoum.edu.eg>
2025-04-04 01:43:39 +0200

    update comming soon shop project

---
+++ coming-soon-shop/README.md
@@ -0,0 +1,29 @@
+<h1 id="title" align="center">Coming Soon Shop</h1>
+
+<p align="center"><img src="https://socialify.git.ci/mostafakamal22/base-apparel-coming-soon-master/image?description=1&amp;language=1&amp;name=1&amp;owner=1&amp;stargazers=1&amp;theme=Auto" alt="project-image"></p>
+
+<p id="description">Coming Soon Shop - Frontend Mentor Challenge</p>
+
+<h2>🚀 Demo</h2>
+
+[https://mostafakamal22.github.io/coming-soon-shop/](https://mostafakamal22.github.io/coming-soon-shop/)
+
+ 
+  
+<h2>🧐 Features</h2>
+
+Here're some of the project's best features:
+
+*   Responsive Design
+*   Landing Page
+*   UI/UX
+
+  
+  
+<h2>💻 Built with</h2>
+
+Technologies used in the project:
+
+*   HTML
+*   CSS
+*   Javascript
--- coming-soon-shop/js/script.js
+++ coming-soon-shop/js/script.js
@@ -1,42 +1,40 @@
 //global variables
-const coverImage = document.getElementById('cover');
-const coverContainer= document.getElementById('cover-container');
-const emailInput =document.getElementById('email');
-const submitBtn= document.getElementById('submit');
-const errorMessage= document.getElementById('error');
-const errorIcon= document.getElementsByClassName('error-icon');
+const coverImage = document.getElementById("cover");
+const coverContainer = document.getElementById("cover-container");
+const emailInput = document.getElementById("email");
+const submitBtn = document.getElementById("submit");
+const errorMessage = document.getElementById("error");
+const errorIcon = document.getElementsByClassName("error-icon");
 
 //replacing cover image at small mobile devices (450px at max)
-function changeCover(){
-    if(window.innerWidth<=450){
-        coverImage.setAttribute('src','./images/hero-mobile.jpg');
-        //setting cover container width to 100%
-        coverContainer.style.width='100%';
-    }
-    else if(window.innerWidth<=800){
-        coverImage.setAttribute('src','./images/hero-desktop.jpg');
-        //setting cover container width to 100%
-        coverContainer.style.width='80%';
-    }
-    else{
-        coverImage.setAttribute('src','./images/hero-desktop.jpg');
-        //setting cover container width to 100%
-        coverContainer.style.width='100%';
-    }
+function changeCover() {
+  if (window.innerWidth <= 450) {
+    coverImage.setAttribute("src", "./images/hero_mobile.webp");
+    //setting cover container width to 100%
+    coverContainer.style.width = "100%";
+  } else if (window.innerWidth <= 800) {
+    coverImage.setAttribute("src", "./images/hero_desktop.webp");
+    //setting cover container width to 100%
+    coverContainer.style.width = "80%";
+  } else {
+    coverImage.setAttribute("src", "./images/hero_desktop.webp");
+    //setting cover container width to 100%
+    coverContainer.style.width = "100%";
+  }
 }
-//Email validation and error message 
-function ValidateEmail(){
-    let mailformat= /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if(emailInput.value.match(mailformat)){
-        errorMessage.innerHTML="";
-        errorIcon[0].style.display="none";
-    }
-    else{
-        errorMessage.innerHTML="please provide a valid email";
-        errorIcon[0].style.display="inline-block";
-        emailInput.focus();
-    }
+//Email validation and error message
+function ValidateEmail() {
+  let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (emailInput.value.match(mailformat)) {
+    errorMessage.innerHTML = "";
+    errorIcon[0].style.display = "none";
+  } else {
+    errorMessage.innerHTML = "please provide a valid email";
+    errorIcon[0].style.display = "inline-block";
+    emailInput.focus();
+  }
 }
-window.addEventListener('DOMContentLoaded',changeCover);
-submitBtn.addEventListener('click',ValidateEmail);
-window.addEventListener('resize', changeCover);
+
+window.addEventListener("DOMContentLoaded", changeCover);
+submitBtn.addEventListener("click", ValidateEmail);
+window.addEventListener("resize", changeCover);
